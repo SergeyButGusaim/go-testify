25e52f58be79f837c2204a46162c20ec22963d31
Бут-Гусаим Сергей <sb21rus@yandex.ru>
2023-12-17 18:02:18 +0300

    Idon'tUnderstand

--- main_test.go
+++ main_test.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func serveHttpRequest(request *http.Request) *httptest.ResponseRecorder {
-	city := "moscow"
-	totalCount := 4
-	url := fmt.Sprintf("/cafe?city=%s&count=%d", city, totalCount)
+func serveHttpRequest(url string) *httptest.ResponseRecorder {
 	req := httptest.NewRequest("GET", url, nil)
 
 	responseRecorder := httptest.NewRecorder()
@@ -22,16 +20,34 @@ func serveHttpRequest(request *http.Request) *httptest.ResponseRecorder {
 }
 
 func TestMainHandlerWhenRequestCorrect(t *testing.T) {
-	want := http.StatusOK
-	have :=
+	responseRecorder := serveHttpRequest("/cafe?count=2&city=moscow")
+	expectedCode := http.StatusOK
+	expectedCount := 2
 
-		assert.Equal(t, want, responseRecorder.Code)
+	assert.Equal(t, expectedCode, responseRecorder.Code)
+	require.NotEmpty(t, responseRecorder.Body)
+	body := strings.Split(responseRecorder.Body.String(), ",")
+	assert.Len(t, body, expectedCount)
 }
 
 func TestMainHandlerWhenCityCorrect(t *testing.T) {
+	expectedBody := `wrong city value`
+	responseRecorder := serveHttpRequest("/cafe?count=2&city=omsk")
+	expectedCode := http.StatusBadRequest
 
+	assert.Equal(t, expectedCode, responseRecorder.Code)
+	require.NotEmpty(t, responseRecorder.Body)
+	body := responseRecorder.Body.String()
+	assert.Equal(t, expectedBody, body)
 }
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	responseRecorder := serveHttpRequest("/cafe?count=10&city=moscow")
+	expectedCode := http.StatusOK
+	expectedCount := 4
 
+	assert.Equal(t, expectedCode, responseRecorder.Code)
+	require.NotEmpty(t, responseRecorder.Body)
+	body := strings.Split(responseRecorder.Body.String(), ",")
+	assert.Len(t, body, expectedCount)
 }
